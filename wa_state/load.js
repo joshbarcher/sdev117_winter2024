196f1c22ed1a2240fa42dbabd613c31efbf48ce4
Josh Archer <jarcher@greenriver.edu>
2024-02-27 09:53:39 -0800

    Made some final changes.

--- wa_state/load.js
+++ wa_state/load.js
@@ -1,16 +1,37 @@
-//load three images!
-for (let i = 1; i <= 5; i++) {
-    loadWaImage();
+//generate random numbers
+let numValidImages = 0;
+const imagesArray = [];
+
+while (numValidImages < 5) {
+    const ranNum = Math.floor(Math.random() * 10) + 1;
+
+    //add to the array if its not already in the array
+    if (!imagesArray.includes(ranNum)) {
+        imagesArray.push(ranNum);
+        numValidImages++;
+    }
+}
+console.log(imagesArray);
+
+for (let imageNum of imagesArray) {
+    loadWaImage(imageNum);
 }
 
-function loadWaImage()
+function loadWaImage(imageNumber)
 {
-    const ranNum = Math.floor(Math.random() * 10) + 1;
+    const anchor = document.createElement("a");
     const image = document.createElement("img");
+    const imageFile = `images/${imageNumber}.png`;
 
-    image.src = `images/${ranNum}.png`;
+    anchor.href = imageFile;
+    image.src = imageFile;
     image.alt = "An interesting place in WA";
 
     const div = document.querySelector("#locations");
-    div.appendChild(image);
+
+    //put the image in the anchor
+    anchor.appendChild(image);
+
+    //put the anchor in the div
+    div.appendChild(anchor);
 }
