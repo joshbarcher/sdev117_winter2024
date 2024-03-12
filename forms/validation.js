193c3ea1e3cc0f0e00e29197680510d50aec5cee
Josh Archer <jarcher@greenriver.edu>
2024-03-12 10:53:12 -0700

    Added manual form validation to an example form.

--- forms/validation.js
+++ forms/validation.js
@@ -1,5 +1,44 @@
 window.onload = () => {
-    const radioButtons = document.querySelectorAll('input[name="gender"]');
+    const button = document.querySelector("button");
+    button.onclick = validate;
+}
+
+function validate(event) {
+    //stop form submission
+    event.preventDefault();
+
+    //validate each part of our form
+    const name = document.querySelector("#name").value;
+    if (name.length < 2 || name.length > 20) { //bad name!
+        document.querySelector("#name-error").style.display = "inline";
+    } else { //good name!
+        document.querySelector("#name-error").style.display = "none";
+    }
+
+    const age = parseInt(document.querySelector("#age").value);
+    console.log(typeof age);
+    if (age < 1 || age > 100) { //bad age!
+        document.querySelector("#age-error").style.display = "inline";
+    } else { //good age!
+        document.querySelector("#age-error").style.display = "none";
+    }
+
+    validateRadioButtons();
+    validateCheckboxes();
+}
+
+function validateCheckboxes() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    for (let element of checkboxes) {
+        if (element.checked) {
+            console.log(element.value);
+        }
+    }
+}
+
+function validateRadioButtons() {
+    const radioButtons = document.querySelectorAll('input[name="funny"]');
     console.log(radioButtons);
 
     //loop over the array of objects
