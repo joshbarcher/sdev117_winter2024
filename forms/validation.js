df2bb61844345ea0f4f0b5416d6cbf6c440cb96c
Josh Archer <jarcher@greenriver.edu>
2024-03-14 09:26:25 -0700

    Added some changes.

--- forms/validation.js
+++ forms/validation.js
@@ -7,24 +7,36 @@ function validate(event) {
     //stop form submission
     event.preventDefault();
 
+    let allValidFields = true;
+
     //validate each part of our form
     const name = document.querySelector("#name").value;
     if (name.length < 2 || name.length > 20) { //bad name!
         document.querySelector("#name-error").style.display = "inline";
+        allValidFields = false;
     } else { //good name!
         document.querySelector("#name-error").style.display = "none";
     }
 
     const age = parseInt(document.querySelector("#age").value);
     console.log(typeof age);
-    if (age < 1 || age > 100) { //bad age!
+    console.log(age);
+    if (Number.isNaN(age) || age < 1 || age > 100) { //bad age!
         document.querySelector("#age-error").style.display = "inline";
+        allValidFields = false;
     } else { //good age!
         document.querySelector("#age-error").style.display = "none";
     }
 
+    //validate other fields...
+
     validateRadioButtons();
     validateCheckboxes();
+
+    //what if things look all right?
+    if (allValidFields) {
+        document.forms["questions"].submit();
+    }
 }
 
 function validateCheckboxes() {
